14eea97984d8cea6d6c814d1a9ae2662db1a1263
Saniul Islam <88367456+saniulislam10@users.noreply.github.com>
2026-01-22 17:53:14 +0600

    Add Google Tag Manager script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-1DQH49VWLB"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-1DQH49VWLB');
+      </script>
       <body>{children}</body>
     </html>
   )
